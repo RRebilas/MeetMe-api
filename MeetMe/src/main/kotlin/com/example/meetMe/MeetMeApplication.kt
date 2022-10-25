4caf1f0df725914f20111293b7bbcbf8d93913eb
RRebilas <rrafal7@gmail.com>
2022-10-25 23:56:29 +0200

    Initial commit with Docker configuration

---
+++ MeetMe/src/main/kotlin/com/example/meetMe/MeetMeApplication.kt
@@ -0,0 +1,23 @@
+package com.example.meetMe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class MeetMeApplication
+
+fun main(args: Array<String>) {
+	runApplication<MeetMeApplication>(*args)
+}
+
+@RequestMapping("/test")
+@RestController
+class TestController {
+	@GetMapping
+	fun testGet(): String {
+		return "test"
+	}
+}
---
+++ MeetMe/src/test/kotlin/com/example/meetMe/MeetMeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.meetMe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MeetMeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
